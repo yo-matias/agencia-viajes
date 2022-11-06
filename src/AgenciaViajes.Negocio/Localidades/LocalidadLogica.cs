46802c292197a9f2cc1d6d80d38b355a84f9fdeb
Matias Frias <matiasnfrias@gmail.com>
2022-11-06 14:49:34 -0300

    Agregadas clases con lógica de negocio para seguridad y localidades

---
+++ src/AgenciaViajes.Negocio/Localidades/LocalidadLogica.cs
@@ -0,0 +1,20 @@
+﻿using AgenciaViajes.Datos;
+using AgenciaViajes.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Negocio.Localidades
+{
+    public static class LocalidadLogica
+    {
+        public static List<PaisModel> ObtenerPaises()
+        {
+            var paises = PaisesRepositorio.ObtenerPaises();
+
+            return paises;
+        }
+    }
+}
---
+++ src/AgenciaViajes.Negocio/Localidades/SeguridadLogica.cs
@@ -0,0 +1,36 @@
+﻿using AgenciaViajes.Datos;
+using AgenciaViajes.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Negocio.Localidades
+{
+    public static class SeguridadLogica
+    {
+        public static bool ValidarUsuario(string usuario, string contraseña)
+        {
+            var usuarioDB = UsuariosRepositorio.ObtenerUsuario(usuario);
+            if (usuario != null)
+            {
+                var contraseñaDB = ObtenerContraseñaVigente(usuarioDB);
+                if (contraseñaDB.Contraseña == contraseña)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static ContraseñaModel ObtenerContraseñaVigente(UsuarioModel usuario)
+        {
+            var contraseña = usuario.Contraseñas.OrderByDescending(c => c.Id)
+                                                .Where(c => c.FechaVencimiento >= DateTime.Today)
+                                                .FirstOrDefault();
+            return contraseña;
+        }
+    }
+}
