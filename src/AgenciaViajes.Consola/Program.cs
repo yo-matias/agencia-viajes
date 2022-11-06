b397875a551930d5dc7b54e69b068d1fe0ec05d0
Matias Frias <matiasnfrias@gmail.com>
2022-11-06 14:56:01 -0300

    Agregado proyecto de consola para realizar pruebas y debug

---
+++ src/AgenciaViajes.Consola/Program.cs
@@ -0,0 +1,40 @@
+﻿using AgenciaViajes.Dominio;
+using AgenciaViajes.Negocio;
+using AgenciaViajes.Negocio.Localidades;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola! Este programa es para pruebas y debug.");
+            Console.WriteLine();
+            
+            // Muestro Países
+            List<PaisModel> paises = LocalidadLogica.ObtenerPaises();
+            if (paises.Any())
+            {
+                Console.WriteLine("Actualmente están cargados los siguientes países:");
+                foreach (var item in paises)
+                {
+                    Console.WriteLine($"-> {item.Pais}");
+                }
+                Console.WriteLine($"Son {paises.Count} países cargados en la base de datos.");
+            }
+            // -
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Presione para salir");
+            Console.ReadLine();
+            
+        }
+    }
+}
