9a8717c305bfe5b4d088c8ad000cdb9873d51fbe
Matias Frias <matiasnfrias@gmail.com>
2022-11-07 17:42:27 -0300

    Agregado esqueleto para lógica de facturación y paquetes

---
+++ src/AgenciaViajes.Negocio/Paquetes/PaquetesLogica.cs
@@ -0,0 +1,17 @@
+﻿using AgenciaViajes.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Negocio.Paquetes
+{
+    public static class PaquetesLogica
+    {
+        public static void CrearPaquete(PaqueteModel paquete)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
