63b8b549f65ff32590542f0d03049733d2e53833
Matias Frias <matiasnfrias@gmail.com>
2022-11-08 00:43:30 -0300

    Agregado repositorio/lógica para Clientes

---
+++ src/AgenciaViajes.Datos/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using AgenciaViajes.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Datos.Repositorios
+{
+    public static class ClienteRepositorio
+    {
+        /// <summary>
+        /// Retorna la lista de todos los Clientes
+        /// </summary>
+        /// <returns>Lista de Clientes</returns>
+        public static List<ClienteModel> ObtenerClientes()
+        {
+            var clientes = new List<ClienteModel>();
+
+            using (var context = new EntidadesDb())
+            {
+                clientes = context.Clientes.ToList();
+            }
+
+            return clientes;
+        }
+
+        /// <summary>
+        /// Retorna la lista de todos los Clientes habilitados
+        /// </summary>
+        /// <returns>Lista de Clientes habilitados</returns>
+        public static List<ClienteModel> ObtenerClientesHabilitados()
+        {
+            using (var context = new EntidadesDb())
+            {
+                var clientes = context.Clientes.Where(c => c.Habilitado).ToList();
+                return clientes;
+            }
+        }
+    }
+}
---
+++ src/AgenciaViajes.Negocio/Clientes/ClientesLogica.cs
@@ -0,0 +1,31 @@
+﻿using AgenciaViajes.Datos.Repositorios;
+using AgenciaViajes.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaViajes.Negocio.Clientes
+{
+    public static class ClientesLogica
+    {
+        /// <summary>
+        /// Obtiene la información de todos los Clientes
+        /// </summary>
+        /// <returns>Lista de Clientes</returns>
+        public static List<ClienteModel> ObtenerClientes()
+        {
+            return ClienteRepositorio.ObtenerClientes();
+        }
+
+        /// <summary>
+        /// Obtiene la información de todos los Clientes habilitados
+        /// </summary>
+        /// <returns>Lista de Clientes</returns>
+        public static List<ClienteModel> ObtenerClientesHabilitados()
+        {
+            return ClienteRepositorio.ObtenerClientesHabilitados();
+        }
+    }
+}
