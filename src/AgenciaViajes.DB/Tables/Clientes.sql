44cbbc7dc449333d265857ef169bc53178fab960
Matias Frias <matiasnfrias@gmail.com>
2022-11-08 00:35:54 -0300

    Agregada fecha alta/baja/activo en Clientes y Usuarios

--- src/AgenciaViajes.DB/Tables/Clientes.sql
+++ src/AgenciaViajes.DB/Tables/Clientes.sql
@@ -8,6 +8,9 @@
     [NombreCliente]   VARCHAR (60) NULL,
     [ApellidoCliente] VARCHAR (60) NULL,
     [Nacionalidad]    VARCHAR (60) NULL,
+    [Habilitado] BIT NOT NULL CONSTRAINT Clientes_ActivoPredeterminado DEFAULT 1, 
+    [FechaAlta] DATETIME NOT NULL CONSTRAINT Clientes_FechaAltaPredeterminado DEFAULT GETDATE(), 
+    [FechaBaja] DATETIME NULL, 
     CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED ([IdCliente] ASC),
     CONSTRAINT [FK_Clientes_Ciudades] FOREIGN KEY ([IdCiudad]) REFERENCES [dbo].[Ciudades] ([IdCiudad]),
     CONSTRAINT [FK_Clientes_TiposCliente] FOREIGN KEY ([IdTipoCliente]) REFERENCES [dbo].[TiposCliente] ([IdTipoCliente])
--- src/AgenciaViajes.DB/Tables/Usuarios.sql
+++ src/AgenciaViajes.DB/Tables/Usuarios.sql
@@ -2,6 +2,9 @@
     [Id]      INT          IDENTITY (1, 1) NOT NULL,
     [Usuario] VARCHAR (50) NOT NULL,
     [Nombre]  VARCHAR (50) NOT NULL,
+    [Habilitado] BIT NOT NULL CONSTRAINT Usuarios_ActivoPredeterminado DEFAULT 1, 
+    [FechaAlta] DATETIME NOT NULL CONSTRAINT Usuarios_FechaAltaPredeterminado DEFAULT GETDATE(), 
+    [FechaBaja] DATETIME NULL,
     CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
