1d565d6d4e61fc606ab6c7bb6721a7f982d6ecdf
Matias Frias <matiasnfrias@gmail.com>
2022-11-09 22:26:14 -0300

    Ajuste datos iniciales de cliente para mayor variedad

--- src/AgenciaViajes.DB/AltaInicial/AltaInicial_Clientes.sql
+++ src/AgenciaViajes.DB/AltaInicial/AltaInicial_Clientes.sql
@@ -6,22 +6,22 @@ VALUES (1,1,'Calle 1', '111', '123456',1,null,'Nombre 1', 'Apellido 1', 1, '1122
 
 INSERT INTO [dbo].[Clientes]
 ([IdCliente],[IdCiudad],[Calle],[NroCalle],[Telefono],[IdTipoCliente],[RazónSocial],[NombreCliente],[ApellidoCliente],[DocumentoTipoId],[DocumentoNro],[CUIT],[NacionalidadId],[Habilitado],[FechaAlta],[FechaBaja])
-VALUES (2,2,'Calle 2', '222', '654321',1,null,'Nombre 1', 'Apellido 1', 1, '11222333', null, 1, 1, GETDATE(), null);
+VALUES (2,11,'Calle 2', '222', '654321',1,null,'Nombre 1', 'Apellido 1', 1, '11222333', null, 1, 1, GETDATE(), null);
 
 INSERT INTO [dbo].[Clientes]
 ([IdCliente],[IdCiudad],[Calle],[NroCalle],[Telefono],[IdTipoCliente],[RazónSocial],[NombreCliente],[ApellidoCliente],[DocumentoTipoId],[DocumentoNro],[CUIT],[NacionalidadId],[Habilitado],[FechaAlta],[FechaBaja])
-VALUES (3,3,'Calle 3', '333', '112233',1,null,'Nombre 1', 'Apellido 1', 1, '11222333', null, 1, 1, GETDATE(), null);
+VALUES (3,6,'Calle 3', '333', '112233',1,null,'Nombre 1', 'Apellido 1', 1, '11222333', null, 1, 1, GETDATE(), null);
 
 INSERT INTO [dbo].[Clientes]
 ([IdCliente],[IdCiudad],[Calle],[NroCalle],[Telefono],[IdTipoCliente],[RazónSocial],[NombreCliente],[ApellidoCliente],[DocumentoTipoId],[DocumentoNro],[CUIT],[NacionalidadId],[Habilitado],[FechaAlta],[FechaBaja])
-VALUES (4,3,'Calle 4', '444', '332211',2,'Amoblamiento Pérez',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
+VALUES (4,8,'Calle 4', '444', '332211',2,'Amoblamiento Pérez',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
 
 INSERT INTO [dbo].[Clientes]
 ([IdCliente],[IdCiudad],[Calle],[NroCalle],[Telefono],[IdTipoCliente],[RazónSocial],[NombreCliente],[ApellidoCliente],[DocumentoTipoId],[DocumentoNro],[CUIT],[NacionalidadId],[Habilitado],[FechaAlta],[FechaBaja])
-VALUES (5,4,'Calle 5', '555', '778899',2,'El Coso',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
+VALUES (5,13,'Calle 5', '555', '778899',2,'El Coso',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
 
 INSERT INTO [dbo].[Clientes]
 ([IdCliente],[IdCiudad],[Calle],[NroCalle],[Telefono],[IdTipoCliente],[RazónSocial],[NombreCliente],[ApellidoCliente],[DocumentoTipoId],[DocumentoNro],[CUIT],[NacionalidadId],[Habilitado],[FechaAlta],[FechaBaja])
-VALUES (6,5,'Calle 6', '666', '995511',2,'La Chiquitita',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
+VALUES (6,2,'Calle 6', '666', '995511',2,'La Chiquitita',null,null, null,null, '11222333', 1, 1, GETDATE(), null);
 
 SET IDENTITY_INSERT [dbo].[Clientes] OFF;
